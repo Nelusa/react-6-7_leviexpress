035296f7eb741eba647418e7257f4b4d92f8173a
Nela Letochová <letochova@oksystem.cz>
2024-05-11 19:38:42 +0200

    feat(project): Part 1 | 3.2 - Seat display

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,6 +1,7 @@
 import { JourneyPicker } from '../../components/JourneyPicker';
 import {useState} from "react";
 import JourneyDetail from "../../components/JourneyDetail/index.jsx";
+import SelectedSeat from "../../components/SelectedSeat/index.jsx";
 
 export const HomePage = () => {
   const [journey, setJourney] = useState(null);
@@ -9,11 +10,14 @@ export const HomePage = () => {
     setJourney(journeyObject);
   }
 
+  console.log(journey)
+
   return (
     <main>
       <JourneyPicker onJourneyChange={handleJourneyChange} />
       {/*{journey && <p>Nalezeno spojení s id {journey.journeyId}</p>}*/}
       {journey && <JourneyDetail journey={journey} />}
+      {journey && <SelectedSeat number={journey.autoSeat} />}
     </main>
   );
 };
