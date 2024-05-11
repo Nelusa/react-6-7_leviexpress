eb76e89cdfe3e5259a02490411ed2bfcd36ed2f5
Nela Letochová <letochova@oksystem.cz>
2024-05-11 15:39:03 +0200

    feat(project): Part 1 - App setup, routing

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,27 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
 import { App } from './components/App';
 import './global.css';
+import {HomePage} from "./pages/HomePage/index.jsx";
+import ReservationPage from "./pages/ReservationPage/index.jsx";
+
+/*Uvnitř komponenty App již máte připravenou strukturu s hlavičkou a patičkou stránky. Mezi ně budeme pomocí Outlet vkládat naše stránky. Stránku HomePage již míte připravenou. Vložte ji do routeru jako dítě routy / v hlavním index.jsx. Stránku HomePage chceme umístit na adresu: /. Vyzkoušejte, že se vaše stránka správně zobrazuje. Měli byste vidět formulář pro vyhledání spojení.
+
+Dále vytvořte komponentu ReservationPage. Tuto komponentu zobrazte na adrese /reservation. Zatím může také vracet pouze nadpis h2, abychom viděli, že se na stránce něco děje. Obsah komponenty vytvoříme později.*/
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      }
+    ],
+  },
+  {
+    path: '/reservation',
+    element: <ReservationPage/>,
   },
 ]);
 
---
+++ src/pages/ReservationPage/index.jsx
@@ -0,0 +1,5 @@
+const ReservationPage = () => {
+  return <h2>Reservation</h2>;
+}
+
+export default ReservationPage;
